92bd8175a694067f8da9dcfc34731ae2440f3e0d
r4gamia <r4gamia@gmail.com>
2022-10-07 15:55:52 +0000

    fix bug

--- resources/views/frontend/accueil.blade.php
+++ resources/views/frontend/accueil.blade.php
@@ -1,138 +1,107 @@
 @extends('frontend.layout.main')
 @section('content')
-@include('frontend.layout.header-sub-menu')
-<section>
-    <div class="rows pad-bot-redu tb-space">
-        <div class="container">
-            <!-- TITLE & DESCRIPTION -->
-            <div class="spe-title">
-                <h2>Nos <span>Guichets de financement</span></h2>
-                <div class="title-line">
-                    <div class="tl-1"></div>
-                    <div class="tl-2"></div>
-                    <div class="tl-3"></div>
-                </div>
-                <p>le guichet unique de financement de l'agence emploi jeunes</p>
-            </div>
-            <div>
-                <!-- TOUR PLACE 1 -->
-                <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow slideInUp" data-wow-duration="0.5s">
-                    <a href="{{ route('guichet1') }}">
-                    <!-- OFFER BRAND -->
-                    <div class="band"> <img src="{{asset('frontend/mages/band.png')}}" alt="" /> </div>
-                    <!-- IMAGE -->
-
-                    <div class="v_place_img"> <img src="{{asset('frontend/images/t5.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
-                    <!-- TOUR TITLE & ICONS -->
-                    <div class="b_pack rows">
-                        <!-- TOUR TITLE -->
-                        <div class="col-md-8 col-sm-8">
-                            <h4><a href="{{route('guichet1')}}">GUICHET 1<span class="v_pl_name"><!-- (Brazil) --></span></a></h4>
-
-                    <div class="v_place_img"> <img src="{{ asset('frontend/images/guichet/guichet1.jpeg') }}" alt="Tour Booking" title="GUICHET 1" /> </div>
-                    <!-- TOUR TITLE & ICONS -->
-                    <div class="b_pack rows">
-                        <!-- TOUR TITLE -->
-                        <div class="col-md-12 col-sm-12">
-                            <h4><a href="{{ route('guichet1') }}">GUICHET 1: Business Angel<span class="v_pl_name"><!-- (Brazil) --></span></a></h4>
-
-                        </div>
-                        <!-- TOUR ICONS -->
-
+    @include('frontend.layout.header-sub-menu')
+    <section>
+        <div class="rows pad-bot-redu tb-space">
+            <div class="container">
+                <!-- TITLE & DESCRIPTION -->
+                <div class="spe-title">
+                    <h2>Nos <span>Guichets de financement</span></h2>
+                    <div class="title-line">
+                        <div class="tl-1"></div>
+                        <div class="tl-2"></div>
+                        <div class="tl-3"></div>
                     </div>
-                    </a>
+                    <p>le guichet unique de financement de l'agence emploi jeunes</p>
                 </div>
-                <!-- TOUR PLACE 2 -->
-                <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="0.7s">
-                    <a href="{{ route('guichet2') }}">
-                    <!-- OFFER BRAND -->
-                   <!--  <div class="band"> <img src="{{asset('frontend/images/band1.png')}}" alt="" /> </div> -->
-                    <!-- IMAGE -->
+                <div>
+                    <!-- TOUR PLACE 1 -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow slideInUp" data-wow-duration="0.5s">
+                        <a href="{{ route('guichet1') }}">
+                            <!-- OFFER BRAND -->
+                            <div class="band"> <img src="{{asset('frontend/mages/band.png')}}" alt="" /> </div>
+                            <!-- IMAGE -->
+                            <div class="v_place_img"> <img src="{{asset('frontend/images/t5.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
+                            <!-- TOUR TITLE & ICONS -->
+                            <div class="b_pack rows">
+                                <!-- TOUR TITLE -->
+                                <div class="col-md-8 col-sm-8">
+                                    <h4><a href="{{route('guichet1')}}">GUICHET 1: Business Angel<span class="v_pl_name"><!-- (Brazil) --></span></a></h4>
+                                </div>
+                                <!-- TOUR ICONS -->
 
-                    <div class="v_place_img"> <img src="{{asset('frontend/images/t1.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
-                    <!-- TOUR TITLE & ICONS -->
-                    <div class="b_pack rows">
-                        <!-- TOUR TITLE -->
-                        <div class="col-md-8 col-sm-8">
-                            <h4><a href="{{route('guichet2')}}">GUICHET 2<span class="v_pl_name"><!-- (England) --></span></a></h4>
-
-                    <div class="v_place_img"> <img src="{{ asset('frontend/images/guichet/guichet2.jpeg') }}" alt="Tour Booking" title="GUICHET 2" /> </div>
-                    <!-- TOUR TITLE & ICONS -->
-                    <div class="b_pack rows">
-                        <!-- TOUR TITLE -->
-                        <div class="col-md-12 col-sm-12">
-                            <h4><a href="{{ route('guichet2') }}">GUICHET 2: Activité Génératrice de revenu(AGR)<span class="v_pl_name"><!-- (England) --></span></a></h4>
-
-                        </div>
-                        <!-- TOUR ICONS -->
-
+                            </div>
+                        </a>
                     </div>
-                    </a>
-                </div>
-                <!-- TOUR PLACE 3 -->
-                <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="0.9s">
+                    <!-- TOUR PLACE 2 -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="0.7s">
+                        <a href="{{ route('guichet2') }}">
+                            <!-- OFFER BRAND -->
+                        <!--  <div class="band"> <img src="{{asset('frontend/images/band1.png')}}" alt="" /> </div> -->
+                            <!-- IMAGE -->
+                            <div class="v_place_img"> <img src="{{asset('frontend/images/t1.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
+                            <!-- TOUR TITLE & ICONS -->
+                            <div class="b_pack rows">
+                                <!-- TOUR TITLE -->
+                                <div class="col-md-8 col-sm-8">
+                                    <h4><a href="{{route('guichet2')}}">GUICHET 2: Activité Génératrice de revenu(AGR)<span class="v_pl_name"><!-- (England) --></span></a></h4>
+                                </div>
+                                <!-- TOUR ICONS -->
 
-                    <div class="v_place_img"><img src="{{asset('frontend/images/t2.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
-                    <div class="b_pack rows">
-                        <div class="col-md-8 col-sm-8">
-                            <h4><a href="{{route('guichet3')}}">GUICHET 3<span class="v_pl_name"><!-- (India) --></span></a></h4>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- TOUR PLACE 3 -->
+                    <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="0.9s">
+                        <a href="{{route('guichet3')}}">
+                            <div class="v_place_img"><img src="{{asset('frontend/images/t2.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
+                            <div class="b_pack rows">
+                                <div class="col-md-8 col-sm-8">
+                                    <h4><a href="{{route('guichet3')}}">GUICHET 3: Micro et petite Entreprise (MPE)<span class="v_pl_name"><!-- (India) --></span></a></h4>
+                                </div>
 
-                    <a href="{{ route('guichet3') }}">
-                    <div class="v_place_img"><img src="{{asset('frontend/images/guichet/guichet3.jpeg')}}" alt="Tour Booking" title="GUICHET 3" /> </div>
-                    <div class="b_pack rows">
-                        <div class="col-md-12 col-sm-12">
-                            <h4><a href="{{ route('guichet3') }}">GUICHET 3: Micro et petite Entreprise (MPE)<span class="v_pl_name"><!-- (India) --></span></a></h4>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- TOUR PLACE 4 -->
+                    <div class="d-flex justify-content-center">
+                        <div class="col-md-2 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.1s">
+                        </div>
+                        <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.1s">
+                            <div class="v_place_img"><img src="{{asset('frontend/images/t3.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
+                            <div class="b_pack rows">
+                                <div class="col-md-12 col-sm-12">
+                                    <h4><a href="{{route('guichet4')}}">GUICHET 4: Moyenne Entreprise et Projets Structurant<span class="v_pl_name"><!-- (China) --></span></a></h4>
+                                </div>
 
+                            </div>
+                            </a>
                         </div>
 
-                    </div>
-                    </a>
-                </div>
-                <!-- TOUR PLACE 4 -->
-                <div class="d-flex justify-content-center">
-                    <div class="col-md-2 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.1s">
-                        <a href="{{ route('guichet4') }}">
-                    </div>
-
-                    <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.1s">
-
-                    <div class="v_place_img"><img src="{{asset('frontend/images/t3.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
-
-                    <div class="v_place_img"><img src="{{ asset('frontend/images/guichet/guichet4.jpeg') }}" alt="Tour Booking" title="GUICHET 4" /> </div>
-
-                    <div class="b_pack rows">
-                        <div class="col-md-12 col-sm-12">
-                            <h4><a href="{{route('guichet4')}}">GUICHET 4: Moyenne Entreprise et Projets Structurant<span class="v_pl_name"><!-- (China) --></span></a></h4>
+                        <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.3s">
+                            <div class="v_place_img"><img src="{{asset('frontend/images/t4.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
+                            <a href="{{ route('guichet5') }}">
+                                <div class="b_pack rows">
+                                    <div class="col-md-12 col-sm-12">
+                                        <h4>
+                                            <a href="{{route('guichet5')}}">
+                                                GUICHET 5: Capital d'investisement
+                                                <span class="v_pl_name"><!-- (Andaman) -->
+                                        </span>
+                                            </a>
+                                        </h4>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
 
-                    </div>
-                    </a>
-                </div>
-
-                <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.3s">
-
-                    <div class="v_place_img"><img src="{{asset('frontend/images/t4.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
-
-                    <a href="{{ route('guichet5') }}">
-                    <div class="v_place_img"><img src="{{ asset('frontend/images/guichet/guichet5.jpeg') }}" alt="Tour Booking" title="GUICHET 5" /> </div>
-
-                    <div class="b_pack rows">
-                        <div class="col-md-12 col-sm-12">
-                            <h4><a href="{{route('guichet5')}}">GUICHET 5: Capital d'investisement<span class="v_pl_name"><!-- (Andaman) --></span></a></h4>
+                        <div class="col-md-2 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.1s">
                         </div>
-
                     </div>
-                </div>
 
-                <div class="col-md-2 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.1s">
-                    </div>
-                    </a>
+                    <!-- TOUR PLACE 5 -->
 
-                </div>
-
-                <!-- TOUR PLACE 5 -->
-
-                <!-- TOUR PLACE 6 -->
+                    <!-- TOUR PLACE 6 -->
                 <!-- <div class="col-md-4 col-sm-6 col-xs-12 b_packages wow fadeInUp" data-wow-duration="1.5s">
                     <div class="v_place_img"><img src="{{asset('frontend/images/t6.png')}}" alt="Tour Booking" title="Tour Booking" /> </div>
                     <div class="b_pack rows">
@@ -157,8 +126,8 @@
                         </div>
                     </div>
                 </div> -->
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -6,33 +6,28 @@
                 <div class="foot-sec2">
                     <div>
                         <div class="row">
-                            <div class="col-sm-3 foot-spec foot-com">
+                            <div class="col-sm-4 foot-spec foot-com">
                                 <h4><span>Plan</span> du site</h4>
                                 <p>Accueil</p>
                                 <p>Guichet de financement</p>
                                 <p>Condition d'eligibilité</p>
                                 <p>A propos</p>
                             </div>
-                            <div class="col-sm-3 foot-spec foot-com">
+                          {{--   <div class="col-sm-3 foot-spec foot-com">
                                 <h4><span>Contacts</span></h4>
                                 <p>PLATEAU, BD CLOZEL / IMMEUBLE PERIGNON </p>
-                    <p> <span class="strong">Tél: </span> <span class="highlighted">27 20 21 50 53/27 20 22 80 32</span> </p>
-                    <p> <span class="strong">Fax: </span> <span class="highlighted">27 20 21 12 86</span> </p>
-                            </div>
-                            <div class="col-sm-3 col-md-3 foot-spec foot-com">
+                                <p> <span class="strong">Tél: </span> <span class="highlighted">27 20 21 50 53/27 20 22 80 32</span> </p>
+                                <p> <span class="strong">Fax: </span> <span class="highlighted">27 20 21 12 86</span> </p>
+                            </div> --}}
+                            <div class="col-sm-4 foot-spec foot-com">
                                 <h4><span>Liens</span> Utiles</h4>
                                 <ul class="two-columns">
                                     <li> <a href="{{ route('user.login') }}">Mon espace</a> </li>
                                     <li> <a href="{{ route('user.enregistrer') }}">Inscription jeunes</a> </li>
                                     <li> <a href="{{ route('mentor.form') }}">Inscription Mentor </a> </li>
-                                    <li> <a href="{{ route('guichet1') }}">Business Angel</a> </li>
-                                    <li> <a href="{{ route('guichet2') }}">Activité génératrice</a> </li>
-                                    <li> <a href="{{ route('guichet3') }}">Micro & petite entreprise</a> </li>
-                                    <li> <a href="{{ route('guichet4') }}">Moyenne entreprise & projet</a> </li>
-                                    <li> <a href="{{ route('guichet5') }}">Capital d'investissement </a> </li>
                                 </ul>
                             </div>
-                            <div class="col-sm-3 foot-social foot-spec foot-com">
+                            <div class="col-sm-4 foot-social foot-spec foot-com">
                                 <h4><span>Suivez-nous</span> sur</h4>
                                <!--  <p>Join the thousands of other There are many variations of passages of Lorem Ipsum available</p> -->
                                 <ul>
