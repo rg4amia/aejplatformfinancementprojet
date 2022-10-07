8f6054c020da326b4cb70b2f77d6cd9a2f32071a
r4gamia <r4gamia@gmail.com>
2022-10-07 14:23:52 +0000

    test

--- resources/views/frontend/layout/header-sub-menu.blade.php
+++ resources/views/frontend/layout/header-sub-menu.blade.php
@@ -5,9 +5,9 @@
             <div class="row">
                 <div class="tourz-search-1">
                     <h1>Guichet Unique du financement en Côte d’Ivoire</h1>
-                    <p>
+                    {{-- <p>
                         Experience the various exciting tour and travel packages and Make hotel reservations, find vacation packages, search cheap hotels and events
-                    </p>
+                    </p> --}}
                 </div>
             </div>
         </div>
